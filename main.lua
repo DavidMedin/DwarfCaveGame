50ae56f0807588aef1fc517040a52417be65da94
David <davidmedin03@gmail.com>
2021-08-04 18:19:10 -0500

    fixed edge case, literally

--- main.lua
+++ main.lua
@@ -10,8 +10,8 @@ if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
   require("lldebugger").start()
 end
 local nuklear = require "nuklear"
-require "server/shapes"
 require "server/networking"
+require "server/shapes"
 local lg = love.graphics
 local lw = love.window
 local lp = love.physics
@@ -26,7 +26,7 @@ loadX,loadY = 5,4 --how many chunks to load from the loadOrigin
 local chunkCanvas = lg.newCanvas(chunkSize*loadX,chunkSize*loadY)
 local chunkCanvasPos= {0,0}
 local loadOrigin = vec2{0,0}
-local chunkEdges = {}
+chunkEdges = {}
 local chunkShader = lg.newShader "chunkShader.glsl"--program on the GPU that describes how to render the chunks
 
 function Snap(vec)
@@ -70,6 +70,7 @@ local client = assert(socket.connect("140.190.30.161",20))
 print("connected")
 
 --generate the chunks
+require "server.physics"
 local blankCanvas = lg.newCanvas(chunkSize,chunkSize,{format="r8"})
 blankCanvas:renderTo(function()
 	lg.clear(3/255,0,0)
@@ -101,7 +102,15 @@ for x=1,loadX do
 		lg.setCanvas()
 	end --im doin sum
 end
-
+for k,v in pairs(chunks) do
+	for q,w in pairs(v) do
+		for pixX=0,chunkSize-1 do
+			for pixY=0,chunkSize-1 do
+				debugFunc(Edgy,pixX,pixY,k,q)
+			end
+		end
+	end
+end
 
 --Convolution city Woot Woot!
 function loadChunks(dx,dy)
@@ -167,49 +176,7 @@ function loadChunks(dx,dy)
 	end
 end
 
-function Edgy(pixX,pixY,chunkX,chunkY)--this time, the pix' are starting at 0, I think
-	--this function is called for every pixel in the effective area of a distance function (like the bounding box of a circle)
-	if chunkEdges[chunkX][chunkY][pixX] ~= nil then
-		if chunkEdges[chunkX][chunkY][pixX][pixY] ~= nil then chunkEdges[chunkX][chunkY][pixX][pixY] =nil end
-	end
-	if chunks[chunkX][chunkY]:getPixel(pixX,pixY) ~= 1/255 then--we don't care if the pixel in question is air
-		local dirs = {vec2{1,0},vec2{1,1},vec2{0,1},vec2{-1,1},vec2{-1,0},vec2{-1,-1},vec2{0,-1},vec2{1,-1}}--will do this better with math :)
-		for k,v in pairs(dirs) do
-			local pixPlace = vec2(pixX,pixY)+v
-			--local chunkPlace = Snap((vec2(chunkX,chunkY)-1)*chunkSize+pixPlace-1)
-			local chunkPlace = ((vec2(chunkX,chunkY)-1)*chunkSize+pixPlace)/chunkSize+1
-			chunkPlace = vec2(math.floor(chunkPlace.x),math.floor(chunkPlace.y))
-			pixPlace = wrap(0,pixPlace,99)
-			local neighbor = {chunks[chunkPlace.x][chunkPlace.y]:getPixel(pixPlace.x,pixPlace.y)}
-			--neighbor should be every neighboring pixel
-			if neighbor[1] == 1/255 then
-				pixPlace = vec2(pixX,pixY)
-				chunkPlace = vec2(chunkX,chunkY)
-				--idk just put it in chunkEdges for now
-				--this garbo below is to initialize chunkEdges in this pixel
-				local indicies = {chunkPlace.x,chunkPlace.y,pixPlace.x}
-				local nextThing = chunkEdges
-				for k,v in pairs(indicies) do
-					if nextThing[v] == nil then
-						nextThing[v] = {}
-					end
-					nextThing = nextThing[v]
-				end
-				chunkEdges[chunkPlace.x][chunkPlace.y][pixPlace.x][pixPlace.y] = 1
-				--chunks[chunkPlace.x][chunkPlace.y]:setPixel(pixPlace.x,pixPlace.y,2/255,0,0)
-				--lg.setCanvas(chunkCanvas)
-				--lg.setShader(chunkShader)
-				--local k = chunkPlace.x
-				--local q = chunkPlace.y
-				--chunkImages[k][q]:release()
-				--chunkImages[k][q] = lg.newImage(chunks[chunkPlace.x][chunkPlace.y])
-				--lg.draw(chunkImages[k][q],(k-1)*chunkSize-chunkCanvasPos[1],(q-1)*chunkSize-chunkCanvasPos[2])
-				--lg.setCanvas()
-				--lg.setShader()
-			end
-		end
-	end
-end
+
 
 function love.mousemoved(x,y,dx,dy,istouch)
 	if not ui:mousemoved(x, y, dx, dy, istouch) then
---
+++ server/physics.lua
@@ -0,0 +1,44 @@
+function Edgy(pixX,pixY,chunkX,chunkY)--this time, the pix' are starting at 0, I think
+	--this function is called for every pixel in the effective area of a distance function (like the bounding box of a circle)
+	if chunkEdges[chunkX][chunkY][pixX] ~= nil then
+		if chunkEdges[chunkX][chunkY][pixX][pixY] ~= nil then chunkEdges[chunkX][chunkY][pixX][pixY] =nil end
+	end
+	if chunks[chunkX][chunkY]:getPixel(pixX,pixY) ~= 1/255 then--we don't care if the pixel in question is air
+		local dirs = {vec2{1,0},vec2{1,1},vec2{0,1},vec2{-1,1},vec2{-1,0},vec2{-1,-1},vec2{0,-1},vec2{1,-1}}--will do this better with math :)
+		for k,v in pairs(dirs) do
+			local pixPlace = vec2(pixX,pixY)+v
+			--local chunkPlace = Snap((vec2(chunkX,chunkY)-1)*chunkSize+pixPlace-1)
+			local chunkPlace = ((vec2(chunkX,chunkY)-1)*chunkSize+pixPlace)/chunkSize+1
+			chunkPlace = vec2(math.floor(chunkPlace.x),math.floor(chunkPlace.y))
+			if chunks[chunkPlace.x] == nil or chunks[chunkPlace.x][chunkPlace.y] == nil then return end
+			pixPlace = wrap(0,pixPlace,99)
+			local neighbor = {chunks[chunkPlace.x][chunkPlace.y]:getPixel(pixPlace.x,pixPlace.y)}
+			--neighbor should be every neighboring pixel
+			if neighbor[1] == 1/255 then
+				pixPlace = vec2(pixX,pixY)
+				chunkPlace = vec2(chunkX,chunkY)
+				--idk just put it in chunkEdges for now
+				--this garbo below is to initialize chunkEdges in this pixel
+				local indicies = {chunkPlace.x,chunkPlace.y,pixPlace.x}
+				local nextThing = chunkEdges
+				for k,v in pairs(indicies) do
+					if nextThing[v] == nil then
+						nextThing[v] = {}
+					end
+					nextThing = nextThing[v]
+				end
+				chunkEdges[chunkPlace.x][chunkPlace.y][pixPlace.x][pixPlace.y] = 1
+				--chunks[chunkPlace.x][chunkPlace.y]:setPixel(pixPlace.x,pixPlace.y,2/255,0,0)
+				--lg.setCanvas(chunkCanvas)
+				--lg.setShader(chunkShader)
+				--local k = chunkPlace.x
+				--local q = chunkPlace.y
+				--chunkImages[k][q]:release()
+				--chunkImages[k][q] = lg.newImage(chunks[chunkPlace.x][chunkPlace.y])
+				--lg.draw(chunkImages[k][q],(k-1)*chunkSize-chunkCanvasPos[1],(q-1)*chunkSize-chunkCanvasPos[2])
+				--lg.setCanvas()
+				--lg.setShader()
+			end
+		end
+	end
+end
